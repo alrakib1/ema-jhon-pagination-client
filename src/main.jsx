7459a9310073b88c15c3266f86160404f6c26b2b
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-11 01:40:44 +0600

    pagination created

---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Checkout from "./components/Checkout/Checkout";
+import Inventory from "./components/Inventory/Inventory";
+import Home from "./components/Layout/Home";
+import Login from "./components/Login/Login";
+import Orders from "./components/Orders/Orders";
+import Shop from "./components/Shop/Shop";
+import "./index.css";
+import cartProductsLoader from "./loaders/cartProductsLoader";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+        loader: () => fetch("http://localhost:5000/productsCount"),
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>,
+        loader: cartProductsLoader,
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "checkout",
+        element: <Checkout></Checkout>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
